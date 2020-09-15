2f3f9b1dd52284552324f1c3c745a94e4e5e933a
KiHyeon-Hong <70433404+KiHyeon-Hong@users.noreply.github.com>
2020-09-15 13:14:19 +0900

    눈 깜빡임 데이터 추출
    눈 깜빡임 데이터 추출

--- Identification.js
+++ Identification.js
@@ -15,87 +15,87 @@ var identification = function(fp1, fp2) {
   //var min = Math.min.apply(null, fp2Array);
   //console.log(min)
 
-  var start = [];
-  var middle = [];
-  var finish = [];
+  var fp1Start = [];
+  var fp1Middle = [];
+  var fp1Finish = [];
+
+  var fp2Start = [];
+  var fp2Middle = [];
+  var fp2Finish = [];
 
   var temp = 0;
-
   var index = 0;
 
   for(var i = 0; i < fp1Array.length; i++) {
     if(temp + 150 < i && fp1Array[i] > BLINKPOINT) {
       for(var j = i; j >= 0; j--) {
         if(fp1Array[j] <= 0) {
-          start[index] = j;
+          fp1Start[index] = j;
           break;
         }
       }
 
-      for(var j = start[index] + 1; j < fp1Array.length; j++) {
+      for(var j = fp1Start[index] + 1; j < fp1Array.length; j++) {
         if(fp1Array[j] <= 0) {
-          middle[index] = j;
+          fp1Middle[index] = j;
           break;
         }
       }
 
-      for(var j = middle[index] + 1; j < fp1Array.length; j++) {
+      for(var j = fp1Middle[index] + 1; j < fp1Array.length; j++) {
         if(fp1Array[j] >= 0) {
-          finish[index] = j;
+          fp1Finish[index] = j;
           break;
         }
       }
 
-      i = finish[index];
+      i = fp1Finish[index];
       index++;
       temp = i;
     }
   }
+
+  index = 0;
+  temp = 0;
 
   for(var i = 0; i < fp2Array.length; i++) {
     if(temp + 150 < i && fp2Array[i] > BLINKPOINT) {
       for(var j = i; j >= 0; j--) {
         if(fp2Array[j] <= 0) {
-          start[index] = j;
+          fp2Start[index] = j;
           break;
         }
       }
 
-      for(var j = start[index] + 1; j < fp2Array.length; j++) {
+      for(var j = fp2Start[index] + 1; j < fp2Array.length; j++) {
         if(fp2Array[j] <= 0) {
-          middle[index] = j;
+          fp2Middle[index] = j;
           break;
         }
       }
 
-      for(var j = middle[index] + 1; j < fp2Array.length; j++) {
+      for(var j = fp2Middle[index] + 1; j < fp2Array.length; j++) {
         if(fp2Array[j] >= 0) {
-          finish[index] = j;
+          fp2Finish[index] = j;
           break;
         }
       }
 
-      i = finish[index];
+      i = fp2Finish[index];
       index++;
       temp = i;
     }
   }
 
-  console.log('start : ' + start);
-  console.log('middle : ' + middle);
-  console.log('finish : ' + finish);
+  console.log('start : ' + fp1Start.length);
+  console.log('middle : ' + fp1Middle.length);
+  console.log('finish : ' + fp1Finish.length);
 
   console.log();
 
-  console.log('start : ' + start.length);
-  console.log('middle : ' + middle.length);
-  console.log('finish : ' + finish.length);
-
-  console.log('start : ' + start.length);
-  console.log('middle : ' + middle.length);
-  console.log('finish : ' + finish.length);
-
-
+  console.log('start : ' + fp2Start.length);
+  console.log('middle : ' + fp2Middle.length);
+  console.log('finish : ' + fp2Finish.length);
 }
 
 exports.identification = identification;
