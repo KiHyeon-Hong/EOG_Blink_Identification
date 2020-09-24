df8f7ac1d159788cadc07f046e818a856fc7cbb6
KiHyeon-Hong <70433404+KiHyeon-Hong@users.noreply.github.com>
2020-09-24 15:21:19 +0900

    config.json 파일 테스트 추가
    config.json 파일 테스트 추가

---
+++ configTest.js
@@ -0,0 +1,8 @@
+// var obj = {'HP':tempHP, 'LP':tempLP, 'LHPL':tempLHPL:tempBlink};
+// obj = JSON.stringify(obj);
+//
+// fs.writeFileSync('./files/data1.json', token1, 'utf8');
+//
+// var originToken2 = fs.readFileSync('./files/data2.json', 'utf8');
+//
+// originToken1 = JSON.parse(originToken1);
