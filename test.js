b1c4d983c54f170db5473ebbc8e1ae69153ae548
KiHyeon-Hong <70433404+KiHyeon-Hong@users.noreply.github.com>
2020-10-06 16:15:40 +0900

    user1 데이터 측정
    user1 데이터 측정

--- test.js
+++ test.js
@@ -6,6 +6,46 @@
   @개인식별 성능 평가를 위한 코드 작성
 */
 
+// const fs = require('fs');
+// const identification = require('./Identification.js');
+// const register = require('./Register.js');
+//
+// var sucSuc = 0;
+// var sucFail = 0;
+// var fail = 0;
+//
+// for(let i = 0; i < 2; i++){
+//   for(let j = 0; j < 10; j++){
+//     let regFile0 = fs.readFileSync('./EOGSampleFile/user' + (i+1) + '/file' + j + '-1.txt', 'utf8');
+//     let regFile1 = fs.readFileSync('./EOGSampleFile/user' + (i+1) + '/file' + j + '-2.txt', 'utf8');
+//
+//     register.register(regFile0, regFile1);
+//
+//     for(let k = 0; k < 2; k++){
+//       for(let l = 0; l < 10; l++){
+//         console.log('user' + (i+1) + '과 user' + (k+1) + ' 비교');
+//         let fp1 = fs.readFileSync('./EOGSampleFile/user' + (k+1) + '/file' + l + '-1.txt', 'utf8');
+//         let fp2 = fs.readFileSync('./EOGSampleFile/user' + (k+1) + '/file' + l + '-2.txt', 'utf8');
+//
+//         let result1 = identification.identification(fp1, fp2);
+//
+//         if((i == k) && (result1 == true)) {
+//           sucSuc++;
+//         }
+//         else if((i != k) && (result1 != true)) {
+//           sucFail++;
+//         }
+//         else {
+//           fail++;
+//         }
+//       }
+//     }
+//   }
+// }
+
+
+
+
 const fs = require('fs');
 const identification = require('./Identification.js');
 const register = require('./Register.js');
@@ -42,6 +82,7 @@ for(let i = 0; i < 2; i++){
     }
   }
 }
+
 
 console.log("sucSuc : " + sucSuc);
 console.log("sucFail : " + sucFail);
